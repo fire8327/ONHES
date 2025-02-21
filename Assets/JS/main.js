3f9be0dec94cd2647edfab72ffed0ef27a7083be
fire8327 <fire83274@gmail.com>
2025-02-21 10:36:38 +0300

    end modals

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -10,3 +10,18 @@ $(".dropdown").each((i, el) => {
 $( function() {
     $( "#tabs" ).tabs();
 } );
+
+/* modals */
+for (let i = 1; i < 10; i++) {
+    $(`#openModal${i}`).click(() => {
+        $(`#modal${i}`).removeClass("-translate-y-[1000px]")
+        $(`#modal${i}`).addClass("top-1/2 -translate-y-1/2")
+        $("#overlay").removeClass("hidden")
+    })
+
+    $(`#overlay, #closeModal${i}`).click(() => {
+        $(`#modal${i}`).addClass("-translate-y-[1000px]")
+        $(`#modal${i}`).removeClass("top-1/2 -translate-y-1/2")
+        $("#overlay").addClass("hidden")
+    })
+}
